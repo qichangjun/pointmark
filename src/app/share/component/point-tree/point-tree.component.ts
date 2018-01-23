a5626b057e04c6a9bf840c37333eace5e3ce9896
qichangjun <1061427304@qq.com>
2018-01-23 17:40:23 +0800

    tree css

--- src/app/share/component/point-tree/point-tree.component.ts
+++ src/app/share/component/point-tree/point-tree.component.ts
@@ -4,62 +4,62 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-point-tree',
   templateUrl: './point-tree.component.html',
-  styleUrls: ['./point-tree.component.css']
+  styleUrls: ['./point-tree.component.scss']
 })
 export class PointTreeComponent implements OnInit ,AfterViewInit{
   @ViewChild('tree') tree:any;
   @Output() changeNode : EventEmitter<any> = new EventEmitter();
-  @Input()   
-  set nodes(nodes){    
+  @Input()
+  set nodes(nodes){
     this._nodes = nodes;
     if (this._nodes.length == 0 || !this.parameter.id){
-      return 
-    }        
+      return
+    }
     setTimeout(()=>{
-      let node = this.tree.treeModel.getNodeById(this.parameter.id);        
+      let node = this.tree.treeModel.getNodeById(this.parameter.id);
       if (!node){
         console.error('没有在树节点中找到id为' + this.parameter.id + '的节点')
         return
       }
       this.expandNode(node);
       this.tree.treeModel.setFocusedNode(node)
-    })    
+    })
   };
   _nodes = [];
   options = {animateExpand:true,childrenField:'childList'}
-  
+
   parameter : Params = {}
   constructor(
     private route: ActivatedRoute,
     private router: Router
   ) {
-    
+
    }
 
   ngOnInit(){
-    
+
   }
 
   ngAfterViewInit() {
     this.route.queryParams
-      .subscribe((param : Params)=>{        
-        if (!param.id){          
+      .subscribe((param : Params)=>{
+        if (!param.id){
           this.tree.treeModel.activeNodeIds = {}
           this.tree.treeModel.focusedNodeId = null;
         }else{
-          this.parameter.id = param.id     
-          let node = this.tree.treeModel.getNodeById(this.parameter.id);        
+          this.parameter.id = param.id
+          let node = this.tree.treeModel.getNodeById(this.parameter.id);
           if (!node){
             console.error('没有在树节点中找到id为' + this.parameter.id + '的节点')
             return
           }
           this.expandNode(node);
-          this.tree.treeModel.setFocusedNode(node)                    
+          this.tree.treeModel.setFocusedNode(node)
         }
       })
   }
 
-  expandNode(node){        
+  expandNode(node){
     node.expand()
     if (node.parent){
       this.expandNode(node.parent)
